af1fdcb937fbdfa1b07ec135b970b66a9c9c6e97
VSharon1 <vsharon1@pm.me>
2025-01-08 18:53:43 +0100

    2704 - done

---
+++ 3-2704-to-be-or-not-to-be.js
@@ -0,0 +1,19 @@
+const expect = (val) => {
+    // function must return an object containing two functions.
+    return {
+        toBe: (val2) => {
+            if (val === val2) {
+                return true;
+            } else {
+                throw new Error ("Not Equal");
+            }
+        },
+        notToBe: (val2) => {
+            if (val !== val2) {
+                return true;
+            } else {
+                throw new Error ("Equal");
+            }
+        }
+    }
+};
