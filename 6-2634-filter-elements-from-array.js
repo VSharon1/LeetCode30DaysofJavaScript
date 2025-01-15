62929b32b6ef178c45c9abf7ec05e745652826da
VSharon1 <vs1.github@pm.me>
2025-01-15 19:19:08 +0100

    2634 - done

---
+++ 6-2634-filter-elements-from-array.js
@@ -0,0 +1,20 @@
+const filter = (arr, fn) => {
+	const result = [];
+	
+	// First Solution:
+	for (const i in arr) {
+		// Number(i), because otherwise it will be a string if you iterate through
+		if (fn(arr[i], Number(i))) {
+			result.push(arr[i]);
+		}
+	}
+	
+	// Second Solution:
+	// arr.forEach((value, index) => {
+	// 	if (fn(value, index)) {
+	// 		result.push(value);
+    //     }
+    // });
+	
+	return result
+};
