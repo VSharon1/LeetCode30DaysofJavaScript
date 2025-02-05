db2e9e4d608e0354128f44e82e9a34cf73e05ff9
Sharon Velikkakath <vsharon1.innovation@gmail.com>
2025-02-05 18:54:21 +0100

    2725 - done

---
+++ 15-2725-interval-cancellation.js
@@ -0,0 +1,13 @@
+const cancellable = (fn, args, t) => {
+	fn(...args);
+	
+	const intervalTimer = setInterval(() => {
+		fn(...args)
+	}, t);
+	
+	const cancelFn = () => {
+		clearInterval(intervalTimer);
+		};
+	
+	return cancelFn;
+};
