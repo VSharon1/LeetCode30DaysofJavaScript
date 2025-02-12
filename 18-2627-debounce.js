4403101cbb8858c15b2431e2a799bbdccdcbb90d
VSharon1 <vsharon1@pm.me>
2025-02-12 07:56:02 +0100

    2627 - done

---
+++ 18-2627-debounce.js
@@ -0,0 +1,11 @@
+const debounce = (fn, t) => {
+    let timeoutId;
+    
+    return (...args) => {
+        clearTimeout(timeoutId);
+
+        timeoutId = setTimeout(() => {
+            fn(...args);
+        }, t)
+    }
+};
