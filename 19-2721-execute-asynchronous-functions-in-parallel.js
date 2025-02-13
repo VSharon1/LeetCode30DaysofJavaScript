4c21acaeab75c84d736d7655490a28983430c7a7
VSharon1 <vs1.github@pm.me>
2025-02-13 15:56:39 +0100

    2721 - done

---
+++ 19-2721-execute-asynchronous-functions-in-parallel.js
@@ -0,0 +1,23 @@
+const promiseAll = (functions) => {
+    return new Promise(async(resolve, reject) => {
+        const lengthFunc = functions.length;
+        const answer = new Array(lengthFunc);
+        let counter = 0;
+
+        functions.forEach(async (func, index) => {
+            try {
+                const value = await func();
+
+                answer[index] = value;
+
+                counter++;
+
+                if (lengthFunc === counter) {
+                    resolve(answer)
+                }
+            } catch (error) {
+                reject(error);
+            }
+        });
+    })
+};
