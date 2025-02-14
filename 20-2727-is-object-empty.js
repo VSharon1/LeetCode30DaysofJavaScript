713eb4f11936d4af509e2d2a7f7354002f0dcbe3
Sharon Velikkakath <vsharon1.innovation@gmail.com>
2025-02-14 09:45:29 +0100

    2727 - done

---
+++ 20-2727-is-object-empty.js
@@ -0,0 +1,15 @@
+const isEmpty = (obj) => {
+    if (typeof(obj) === "object") {
+        if (Object.keys(obj).length === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        if (obj.length === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
