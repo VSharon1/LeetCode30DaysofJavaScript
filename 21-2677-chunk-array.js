f9a4d476558674140aed4c86b9500804ac2739ec
VSharon1 <vs1.github@pm.me>
2025-02-15 08:09:56 +0100

    2677 - done

---
+++ 21-2677-chunk-array.js
@@ -0,0 +1,19 @@
+const chunk = (arr, size) => {
+    const result = [];
+    let subArray = [];
+
+    for (let index = 0; index < arr.length; index++) {
+        subArray.push(arr[index]);
+
+        if (subArray.length === size) {
+            result.push(subArray);
+            subArray = [];
+        }
+    }
+
+    if (subArray.length) {
+        result.push(subArray);
+    }
+
+    return result;
+};
