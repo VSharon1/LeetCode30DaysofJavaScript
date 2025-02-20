fb7eb600e22161b4a8adccacd0a2ddb1e92270fd
Sharon Velikkakath <vsharon1.innovation@gmail.com>
2025-02-20 19:05:00 +0100

    2625 - done

---
+++ 26-2625-flatten-deeply-nested-array.js
@@ -0,0 +1,15 @@
+const flat = (arr, n) => {
+    const result = [];
+
+    const helper = (arr, depth) => {
+        for (const value of arr) {
+            if (typeof value === 'object' && depth < n) {
+                helper(value, depth + 1)
+            } else {
+                result.push(value);
+            }
+        }
+        return result;
+    }
+    return helper(arr, 0);
+};
